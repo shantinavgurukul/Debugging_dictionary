d74fb3a4117a4b413246c350f30abdb4930c1fd4
shantinavgurukul <you@example.com>
2020-10-05 22:21:50 +0530

    first commit

---
+++ question1.py
@@ -0,0 +1,10 @@
+details={
+    "name":"Shanti",
+    "age":12,
+    "email":"shanti@navgurukul.org",
+    }
+
+# print(details["name"])
+# print(details["lastname"])
+# print(details[age])
+print(details)
---
+++ question3.py
@@ -0,0 +1,15 @@
+c=dict()
+for i in range(1,16):
+	c[i]=i*i
+print(c) 
+# n = 15
+
+# # declare dictionary 
+# numbers = {}
+
+# # run loop from 1 to n 
+# for i in range(1, n+1):
+# 	numbers[i] = i * i
+
+# # print dictionary 
+# print (numbers)
